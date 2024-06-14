fc1c009db100e573268af3bfe7f185308f3e4eec
Rahul <115698783+architectrahul@users.noreply.github.com>
2024-06-14 09:49:38 +0530

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,15 +26,15 @@ module "sandbox" {
   account_customizations_name = "sandbox"
 }
 
-module "sandbox-1" {
+module "sandboxx" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "steeritsolutions+sandbox-1@gmail.com"
-    AccountName               = "sandbox-aft-1"
+    AccountEmail              = "steeritsolutions+sandbox2@gmail.com"
+    AccountName               = "sandbox-aft-two"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "steeritsolutions+sandbox-1@gmail.com"
-    SSOUserFirstName          = "Sandbox-1"
+    SSOUserEmail              = "steeritsolutions+sandbox2@gmail.com"
+    SSOUserFirstName          = "Sandboxtwo"
     SSOUserLastName           = "AFT"
   }
 
